042f5fb2861c186d81cd26763251d457a342c577
janier2000 <jomr10@hotmail.com>
2026-05-10 11:15:50 -0500

    OJO instalar mudblazor

--- LibraryManagementSystems/LibrarySystem.Frontend/Program.cs
+++ LibraryManagementSystems/LibrarySystem.Frontend/Program.cs
@@ -1,11 +1,31 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using LibrarySystem.Frontend;
+using LibrarySystem.Frontend.Services.Implementations;
+using LibrarySystem.Frontend.Services.Interface;
+using LibrarySystem.Frontend.Utilidad;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//cambio temporal
+builder.Services
+       .AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7131/") });
 
+
+builder.Services.AddSingleton<MenuService>();
+
+builder.Services.AddScoped<IBookServices, BookServices>();
+builder.Services.AddScoped<ICategoryServices, CategoryServices>();
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSweetAlert2();
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
